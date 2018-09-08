61aa29d11399aa80ca550994eebd0188719af45d
AbrahamMtz007 <lighestal009@gmail.com>
2018-09-08 15:48:26 -0500

    Area de circunferencia

---
+++ Homework1/AreaCircunferencia.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AreaCircunferencia{
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Ingrese el radio de la circunferencia");
+    double r = input.nextDouble();
+    double pi;
+    pi = 3.1416;
+    double area;
+    area = pi * r;
+    System.out.println("El area de la circunferencia es: "+ area);
+  }
+}
