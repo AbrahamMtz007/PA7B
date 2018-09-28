34821d38b00ab32d16b8cca6bcbd942b06cf9e5a
AbrahamMtz007 <lighestal009@gmail.com>
2018-09-28 17:19:48 -0500

    ForLoop

--- ForLoop.java
+++ ForLoop.java
@@ -2,15 +2,19 @@ import javax.swing.JOptionPane;
 
 public class ForLoop{
   public static void main(String[] args) {
-
-
+    int pregunta=0;
+    do{
     int tabla = Integer.parseInt(JOptionPane.showInputDialog(null,"enter a tabla"));
-    while(tabla!=0){
+
     String salida="";
     for(int i = 0; i <= 9; i++){
       salida += tabla + "x" + (i + 1) + " = " + (i + 1)*tabla+"\n";
     }
-      JOptionPane.showMessageDialog(null, salida);
-    }
+    JOptionPane.showMessageDialog(null, salida);
+    pregunta = Integer.parseInt(JOptionPane.showInputDialog(null,"¿Quiere realizar otra tabla"));
   }
+
+  while (pregunta !=0 );
+
+    }
   }
