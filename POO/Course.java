b209a598c393e5597e392f8dcf876222cd327f24
AbrahamMtz007 <lighestal009@gmail.com>
2018-10-19 21:51:18 -0500

    Tarea

--- POO/Course.java
+++ POO/Course.java
@@ -3,6 +3,14 @@ public class Course{
   private String[] students = new String[100];
   private int numberOfStudents = 0;
 
+  public Course(){
+
+  }
+
+  public Course(String courseName){
+    this.courseName = courseName;
+  }
+
   public String getCourseName(){
     return this.courseName;
   }
@@ -17,6 +25,8 @@ public class Course{
 
   public void dropStudent(String student){
     //Tarea
+    students=new String[1];
+    numberOfStudents=0;
   }
 
   public String[] getStudents(){
--- POO/CourseImpl.java
+++ POO/CourseImpl.java
@@ -1,20 +1,62 @@
 import javax.swing.*;
 public class CourseImpl{
   public static void main(String[] args) {
-  Course curso1 = new Course();
+   int opc = 0;
+   Course curso1 = new Course();
+    do {
+      opc= Integer.parseInt(JOptionPane.showInputDialog(null,"Seleccione la opcion a realizar \n 1.-Crear Curso \n 2.-Agregar Estudiantes \n 3.-Eliminar Estudiantes \n 4.-Mostrar Estudiantes \n 5.-Mostrar Numero de Estudiantes \n 6.- Salir"));
+      switch(opc){
+        case 1:
+        //crear curso
+          curso1.SetCourseName(JOptionPane.showInputDialog(null,"Ingrese nombre del curso"));
+        break;
+        case 2:
+        //agregar estudiante
+          curso1.addStudent(JOptionPane.showInputDialog(null,"ingrese nombre del estudiante"));
+          curso1.addStudent(JOptionPane.showInputDialog(null,"ingrese nombre del estudiante"));
+          curso1.addStudent(JOptionPane.showInputDialog(null,"ingrese nombre del estudiante"));
+        break;
+        case 3:
+        //eliminar
+          curso1.dropStudent("");
+        break;
+        case 4:
+        //mostrar estudiantes
+        String [] studentsDeCurso1= curso1.getStudents();
+        String students = "";
+        for (int i = 0;i < curso1.getNumberOfStudents() ;i ++ ) {
+          students += studentsDeCurso1[i]+ "\n";
+        }
+        JOptionPane.showMessageDialog(null, students);
+        break;
+        case 5:
+        //mostrar numero de estudiantes
+        JOptionPane.showMessageDialog(null,curso1.getNumberOfStudents());
+        break;
+        default:
+        //mensaje de opcion
+      }
+    } while (opc!=6);
+
+  /*Course curso1 = new Course();
   //curso1.courseName="Inglés";
   curso1.SetCourseName("Inglés");
 
   curso1.addStudent("Luis Alberto");
   curso1.addStudent("Maria");
   curso1.addStudent("Lucero");
+  curso1.dropStudent("");
 
   String [] studentsDeIngles= curso1.getStudents();
   String students = "";
   for (int i = 0;i < curso1.getNumberOfStudents() ;i ++ ) {
     students += studentsDeIngles[i]+ "\n";
+
   }
+
   JOptionPane.showMessageDialog(null, students);
+
+
 
   Course curso2 = new Course();
   curso2.SetCourseName("Mate");
@@ -26,5 +68,12 @@ public class CourseImpl{
   JOptionPane.showMessageDialog(null, curso1.getCourseName());
 
   JOptionPane.showMessageDialog(null, curso2.getCourseName());
+
+  Course curso3 = new Course("Programacion");
+  curso3.addStudent("agustin");
+  curso3.addStudent("Juan Carlos");
+  curso3.addStudent("You");*/
+
+
   }
 }
